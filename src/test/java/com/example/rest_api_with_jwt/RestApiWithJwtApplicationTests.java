db2f9a9e7d886e0d57d6574bb84240305b4a24b5
hyujikoh <dvum0045@gmail.com>
2022-10-05 13:38:50 +0900

    Initial commit

---
+++ src/test/java/com/example/rest_api_with_jwt/RestApiWithJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rest_api_with_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiWithJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
