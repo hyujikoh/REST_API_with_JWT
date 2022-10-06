54786369330b74726607ba885d08cb81d5fba59f
hyujikoh <dvum0045@gmail.com>
2022-10-06 15:14:21 +0900

    로그인 성공시 응답 본문에도 accessToken 넣기

--- src/main/java/com/example/rest_api_with_jwt/app/member/controller/MemberController.java
+++ src/main/java/com/example/rest_api_with_jwt/app/member/controller/MemberController.java
@@ -37,12 +37,19 @@ public class MemberController {
         if (passwordEncoder.matches(postLoginReq.getPassword(), member.getPassword()) == false) {
             return Util.spring.responseEntityOf(RsData.of("F-3", "비밀번호가 일치하지 않습니다."));
         }
-
+        String accessToken = "JWT_Access_Token";
 
         PostLoginRes postLoginRes = memberService.Login(postLoginReq);
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Authentication", "JWT_Access_Token");
-        return Util.spring.responseEntityOf(RsData.of("S-1", "로그인 성공, Access Token을 발급합니다."), headers);
+        return Util.spring.responseEntityOf(
+                RsData.of(
+                        "S-1",
+                        "로그인 성공, Access Token을 발급합니다.",
+                        Util.mapOf(
+                                "accessToken", postLoginRes.getAccessToken()
+                        )
+                ),
+                Util.spring.httpHeadersOf("Authentication", accessToken)
+        );
     }
 
 }
