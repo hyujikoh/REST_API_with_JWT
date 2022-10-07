367a90f01381726b9b934c593340f2c195cab3d5
hyujikoh <dvum0045@gmail.com>
2022-10-07 13:15:59 +0900

    테스트 케이스

--- src/test/java/com/example/rest_api_with_jwt/AuthTests.java
+++ src/test/java/com/example/rest_api_with_jwt/AuthTests.java
@@ -16,6 +16,7 @@ import org.springframework.transaction.annotation.Transactional;
 import java.nio.charset.StandardCharsets;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
