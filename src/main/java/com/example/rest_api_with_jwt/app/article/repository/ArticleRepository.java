2a90b3fe63ea58a428b4ce3a06242f56ae7104cd
hyujikoh <dvum0045@gmail.com>
2022-10-10 22:43:59 +0900

    GET /articles

--- src/main/java/com/example/rest_api_with_jwt/app/article/repository/ArticleRepository.java
+++ src/main/java/com/example/rest_api_with_jwt/app/article/repository/ArticleRepository.java
@@ -3,5 +3,8 @@ package com.example.rest_api_with_jwt.app.article.repository;
 import com.example.rest_api_with_jwt.app.article.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ArticleRepository extends JpaRepository<Article, Long> {
+    List<Article> findAllByOrderByIdDesc();
 }
